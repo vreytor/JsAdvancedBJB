be2c0bda4019a416d90c0ae8107df051d8404243
vreytor <69092748+vreytor@users.noreply.github.com>
2022-12-05 09:37:12 -0500

    CHANGES

---
+++ js/e1s1.js
@@ -0,0 +1,13 @@
+/* Crear una función que convierta los strings que se ingresan como parámetros a estilo capitalizado.
+Ejemplo: "hola a todos" => "Hola A Todos" */
+
+let frases = "hola a todos los humanos"
+
+function capitalizar(frase){
+    let fraseCortada = frase.split("") /* El método split() divide un objeto de tipo String en un array (vector) de cadenas mediante la separación de la cadena en subcadenas. */
+    let newAarray = []
+
+    for(let i = 0; < fraseCortada.length ;i++){
+        
+    }
+}
