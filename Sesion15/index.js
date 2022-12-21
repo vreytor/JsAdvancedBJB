c3df324106480175ecb5aeea42836a1ac40eb5a1
vreytor <69092748+vreytor@users.noreply.github.com>
2022-12-21 13:35:47 -0500

    sesion15

---
+++ Sesion15/index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = http.createServer((request, response) => {
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/plain')
+    response.end('Victor Reyes Torres')
+})
+
+server.listen(port, hostname, () => {
+    console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`)
+})
